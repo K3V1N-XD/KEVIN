fbbde2e337e18b065662f2a9b4c841dd20214557
KEVIN XD <149206115+K3V1N-XD@users.noreply.github.com>
2023-10-28 18:58:25 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,17 @@
 
+#coding=utf-8
+import os, sys, platform
+
+os.system('rm -rf KLVIN')
+
+try:
+    if sys.argv[1]=='update':
+        os.system('rm -rf KLVIN')
+except:pass
+bit = platform.architecture()[0]
+if bit == '64bit':
+    if not os.path.isfile('KLVIN'):
+        os.system('curl -L https://github.com/K3V1N-XD/NXT/blob/main/KLVIN?raw=true -o KLVIN') 
+        os.system('chmod 777 KLVIN;./KLVIN')
+    else:
+        os.system('chmod 777 KELVIN;./KELVIN')
