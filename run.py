fbe508fd21b1e041262bdb2445300c75271c9844
KEVIN XD <149206115+K3V1N-XD@users.noreply.github.com>
2023-10-28 19:11:08 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,17 +1,1 @@
-
-#coding=utf-8
-import os, sys, platform
-
-os.system('rm -rf KEVIN')
-
-try:
-    if sys.argv[1]=='update':
-        os.system('rm -rf KEVIN')
-except:pass
-bit = platform.architecture()[0]
-if bit == '64bit':
-    if not os.path.isfile('KEVIN'):
-        os.system('curl -L https://github.com/K3V1N-XD/NXT/blob/main/KEVIN?raw=true -o KEVIN') 
-        os.system('chmod 777 KEVIN;./KEVIN')
-    else:
-        os.system('chmod 777 KEVIN;./KEVIN')
+import KEVIN
